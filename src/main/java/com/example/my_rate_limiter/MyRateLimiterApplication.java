2f5b68015e055865c466d6939387bb422ee7e912
Luiz Fernando de Andrade Gadelha <luizfernandodag@gmail.com>
2025-10-20 15:28:04 -0300

    Commit inicial do projeto Spring Boot

---
+++ src/main/java/com/example/my_rate_limiter/MyRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_rate_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRateLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_rate_limiter/MyRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_rate_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
